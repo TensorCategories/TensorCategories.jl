95388f347d202936ec5f17c230ff7d0f1576ba33
Ulrich Thiel <ulrich.thiel@math.rptu.de>
2026-08-29 10:21:10 +0200

    Give dormant S-matrix calculations complete references
    Replace bare links with complete citations for Lusztig's exotic Fourier transform and the Geck--Malle comparison matrix. This makes the mathematical provenance of the exploratory formulas explicit while retaining the warning that the file is not included by runtests.jl and supplies no active regression assertions.

--- test/S-MatrixTest.jl
+++ test/S-MatrixTest.jl
@@ -14,8 +14,9 @@ C.simples = C.simples[[2,1,3,4]]
 
 S = normalized_smatrix(C)
 
-# Lusztigs S-matrix according to 
-# https://projecteuclid.org/journals/duke-mathematical-journal/volume-73/issue-1/Exotic-Fourier-transform/10.1215/S0012-7094-94-07309-2.short
+# Lusztig's S-matrix is from G. Lusztig, "Exotic Fourier transform",
+# Duke Math. J. 73 (1994), no. 1, 227--241, with an appendix by G. Malle,
+# pp. 243--248, doi:10.1215/S0012-7094-94-07309-2.
 
 K = base_ring(I)
 ξ = gen(K)
@@ -26,8 +27,9 @@ Lusztigs_S = inv(sqrt(K(5))) * matrix(K,[  λ-1 λ   1    1;
                         1   -1  λ    -λ+1;
                         1   -1  -λ+1 λ])
 
-# S-matrix according to Geck, Malle
-# https://www.sciencedirect.com/science/article/pii/S0021869302006312
+# The comparison matrix is from M. Geck and G. Malle, "Fourier transforms and
+# Frobenius eigenvalues for finite Coxeter groups", J. Algebra 260 (2003),
+# no. 1, 162--193, doi:10.1016/S0021-8693(02)00631-2.
 
 tuples = [(i,j) for i ∈ 0:5, j ∈ 0:5 if 0 < i < j < i+j < 5 || 0 == i < j < 5/2][[1,2,4,3]]
 tuples_2 = [(k,l) for k ∈ 1:2:7, l ∈ 1:2:7 if 0 < k < l < k+l < 10][[3,2,1,4]]
